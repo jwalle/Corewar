29d667691957d6853a35ddce1cf64eb9159fddc4
Romain MICOLON <rmicolon@e3r9p16.42.fr>
2016-08-03 04:27:39 +0200

    carry implemented, instruction norm inp

--- sources/fork.c
+++ sources/fork.c
@@ -6,7 +6,7 @@
 /*   By: jwalle <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/23 18:41:59 by jwalle            #+#    #+#             */
-/*   Updated: 2016/07/23 18:42:06 by jwalle           ###   ########.fr       */
+/*   Updated: 2016/08/03 00:18:03 by rmicolon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,10 @@ char	cw_zjmp(t_cwar *cwar, t_proc *proc)
 	int		new_pc;
 
 	new_pc = get_new_fork_pc(cwar, proc);
-	// if (proc->carry) // uncomment when carry set on other ins
+	if (proc->carry)
 		proc->pc = circ(proc->pc, new_pc);
+	else
+		proc->pc = circ(proc->pc, 3);
 	return (1);
 }
 
